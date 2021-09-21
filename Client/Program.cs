4bfa19cc4a87dfa2869d3f2808885189379a1559
Lars Nysom <LANY@ucn.dk>
2021-09-21 09:21:09 +0200

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using DataAccessLayer;
+using System;
+using System.Data;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SqlConnectionStringBuilder bldr = new SqlConnectionStringBuilder();
+            //bldr.DataSource = @"(localdb)\mssqllocaldb";
+            //bldr.InitialCatalog = "ContosoUniversity";
+            //bldr.IntegratedSecurity = true;
+
+            string connectionString = @"Data source=(localdb)\mssqllocaldb;Initial Catalog=ContosoUniversity;Integrated Security=true;";
+
+            IDbConnection conn = DataContext.GetConnection(connectionString);
+            conn.Open();        
+
+            IDbCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT * FROM Student";
+            IDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.GetString(2) + " " + reader.GetString(1));
+            }
+        }
+    }
+}
---
+++ DataAccessLayer/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccessLayer
+{
+    public static class DataContext
+    {
+        public static IDbConnection GetConnection(string connectionString)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            return conn;
+        }
+    }
+}
